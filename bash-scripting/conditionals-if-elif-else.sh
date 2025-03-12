88fc6624ec32084fc8d4f31cea5523206b14b47e
lakshman8cs <62003533+lakshman8cs@users.noreply.github.com>
2025-03-12 16:17:23 +0530

    Create conditionals-if-elif-else.sh

---
+++ bash-scripting/conditionals-if-elif-else.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# define a variable
+count=3
+
+# check if the count is greater than 5
+if [ $count -gt 5 ]
+then
+  echo "The count is greater than 5."
+elif [ $count -eq 5 ]
+then
+  echo "The count is equal to 5."
+else
+  echo "The count is less than 5."
+fi
